b602a547f8ea1cca9ae842215829677663e562f1
cristinogomez <133212525+cristinogomez@users.noreply.github.com>
2025-01-14 11:20:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 # Show the page title and description.
-st.set_page_config(page_title="Gestión Agendas Cot")
+st.set_page_config(page_title="Gestión Agendas Cot",layout="wide")
 st.title("Agendas COT")
 
 # Load the data from a CSV. We're caching this so it doesn't reload every time the app
