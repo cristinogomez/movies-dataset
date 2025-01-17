bdfaf96df0272d12763b880992b9c8cb8aa17bbc
cristinogomez <133212525+cristinogomez@users.noreply.github.com>
2025-01-17 08:03:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,10 @@ with col2:
             boton_guardar=st.form_submit_button('Save')
             if boton_guardar:
                 with open('data/ACV.csv', mode='w') as file :
+                    df1 = pd.DataFrame({'name': ['Raphael', 'Donatello'],
+                                        'mask': ['red', 'purple'],
+                                        weapon': ['sai', 'bo staff']})
+                    df1.to_csv('out.csv', index=False)  
                     st.write("Edited dataframe:", edited_df)
                     edited_df.to_csv(file, index=False)
                 
